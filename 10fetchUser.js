35a5553b0a2647ac08b70e15b1eaa0a1dfec10df
Lourdes Monahan <lourdes.marija.monahan@gmail.com>
2019-08-31 17:10:59 -0700

    all notes from videos

---
+++ 10fetchUser.js
@@ -0,0 +1,18 @@
+const API = 'https://acme-users-api-rev.herokuapp.com/api';
+
+const fetchUser = async ()=> {
+  const storage = window.localStorage;
+  const userId = storage.getItem('userId');
+  if(userId){
+    try {
+      return (await axios.get(`${API}/users/detail/${userId}`)).data;
+    }
+    catch(ex){
+      storage.removeItem('userId');
+      return fetchUser();
+    }
+  }
+  const user = (await axios.get(`${API}/users/random`)).data;
+  storage.setItem('userId', user.id);
+  return  user;
+};
